6b4d81d1d872cd7929e77ff999202312e8fa4114
Damian Domin <damian.domin334@gmail.com>
2024-07-24 16:03:05 +0200

    Remove clutter

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -5,16 +5,11 @@ import WordleBoard from '../WordleBoard.vue'
 describe('WordleBoard', () => {
   test('a victory message appears when the user makes a guess that matches the word of they day', async () => {
 
-    // Arange
     const wrapper = mount(WordleBoard, { props: { worldOfTheyDay: 'TESTS' } })
-
-    // Act
 
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
-
-    // Assert
 
     expect(wrapper.text()).toContain("You won!")
 
