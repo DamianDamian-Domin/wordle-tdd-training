ea80f35aa6826c18f52d621c449bdc48d8054464
Damian Domin <damian.domin334@gmail.com>
2024-07-24 16:59:01 +0200

    Extract arrange phase to before each

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -5,9 +5,11 @@ import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
   let wordOfTheyDay = "TESTS"
+  let wrapper: ReturnType<typeof mount>;
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheyDay } })
+  })
   test('a victory message appears when the user makes a guess that matches the word of they day', async () => {
-
-    const wrapper = mount(WordleBoard, { props: { wordOfTheyDay } })
 
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
@@ -18,7 +20,6 @@ describe('WordleBoard', () => {
   })
 
   test('a defeat message apperas if the user makes a guess that is incorrect', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheyDay } })
 
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("WRONG")
@@ -28,7 +29,6 @@ describe('WordleBoard', () => {
   })
 
   test("no end of game message appears if the user has not yet made a guess", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheyDay } })
 
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
