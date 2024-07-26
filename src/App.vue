58b6f61e9d711bbc362a0819250f515301a06a5c
Damian Domin <damian.domin334@gmail.com>
2024-07-26 12:25:40 +0200

    Use word of they day

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <WordleBoard word-of-they-day="TESTS" />
+    </div>
+  </header>
+
+</template>
+
 <script setup lang="ts">
 import WordleBoard from './components/WordleBoard.vue'
 </script>
 
-<template>
-  <header>
-    <div class="wrapper">
-      <WordleBoard word-of-they-day="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
 
 <style scoped>
 
-</style>./components/WordleBoard.vue
+</style>
