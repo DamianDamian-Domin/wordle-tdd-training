2343fa88bc52f30bbba639b3fe308fd63f86f403
Damian Domin <damian.domin334@gmail.com>
2024-07-26 15:49:22 +0200

    extract GuessView component

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -1,12 +1,6 @@
 <template>
-    <ul class="word">
-      <li v-for="(letter, index) in guessInProgress.padEnd(WORD_SIZE, ' ')"
-          :key="`${letter}-${index}`"
-          :data-letter="letter"
-          class="letter"
-          v-text="letter"/>
-    </ul>
-  
+    
+    <GuessView :guess="guessInProgress" />
     <input v-model="guessInProgress"
            :maxlength="WORD_SIZE"
            autofocus
@@ -20,6 +14,7 @@
 import { ref } from "vue"
 import { WORD_SIZE } from '@/settings';
 import englishWords from "@/englishWordsWith5Letters.json"
+import GuessView from "@/components/GuessView.vue"
 
 const emit = defineEmits<{
   "guess-submitted": [guess: string]
@@ -53,36 +48,4 @@ input {
   opacity: 0;
 }
 
-.word {
-  list-style: none;
-  padding: 0;
-  display: flex;
-  gap: 0.25rem;
-}
-
-.letter {
-  background-color: white;
-  border: 1px solid hsl(0, 0%, 70%);
-  width: 5rem;
-  height: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
-  font-weight: bolder;
-}
-
-li:not([data-letter=" "]) {
-  animation: pop 100ms;
-}
-
-@keyframes pop {
-  0% {
-    transform: scale(1);
-  }
-
-  50% {
-    transform: scale(1.4);
-  }
-}
 </style>
